13130b7d8f3b7f4f6c12193b2cbe74473232442f
Krish120003 <krish120003@gmail.com>
2025-04-07 15:28:03 -0400

    feat: scaffold code page

--- src/app/[owner]/[repository]/page.tsx
+++ src/app/[owner]/[repository]/page.tsx
@@ -1,3 +1,26 @@
+import RepositoryFileList from "./_components/repository-file-list";
+
 export default function Page() {
-  return <div>Repo/Code Page</div>;
+  // somehow get data
+
+  return (
+    <div className="p-4 mx-auto md:max-w-7xl w-full">
+      <div className="w-full grid grid-cols-4 gap-8">
+        {/* File Browser and README */}
+        <div className="col-span-3 flex flex-col gap-4">
+          <RepositoryFileList />
+
+          <section>Readme goes here i think</section>
+        </div>
+
+        {/* Description / Metadata */}
+        <div className="col-span-1">
+          <section className="">
+            <h2 className="font-bold">About</h2>
+            <p>Repo Description</p>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
