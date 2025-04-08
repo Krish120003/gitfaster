b030b190fdf9b18a7f978b3236ce45e2b872684a
Krish120003 <krish120003@gmail.com>
2025-04-08 02:23:00 -0400

    feat: flat folder view api route

--- src/app/[owner]/[repository]/(explorer)/tree/[branch]/[[...path]]/page.tsx
+++ src/app/[owner]/[repository]/(explorer)/tree/[branch]/[[...path]]/page.tsx
@@ -1,3 +1,43 @@
-export default function Page() {
-  return <div>tree page</div>;
+import { api } from "@/trpc/server";
+
+interface PageProps {
+  params: Promise<{
+    owner: string;
+    repository: string;
+    branch: string;
+    path: string | string[] | undefined;
+  }>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const { owner, repository, branch, path } = await params;
+
+  const formattedPath = Array.isArray(path) ? path.join("/") : path;
+  const data = await api.github.getFolderView({
+    owner,
+    repository,
+    branch,
+    path: formattedPath ?? "",
+  });
+
+  return (
+    <div>
+      <div>
+        params are:
+        <code>
+          <pre>{JSON.stringify(await params, null, 2)}</pre>
+          <pre>{formattedPath}</pre>
+        </code>
+      </div>
+      <ul>
+        {data.map((item) => (
+          <li key={item.path}>
+            <a href={`/${owner}/${repository}/blob/${branch}/${item.path}`}>
+              {item.path}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
