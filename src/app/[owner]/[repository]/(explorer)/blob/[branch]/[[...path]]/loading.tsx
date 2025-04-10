3b895bf262cd6f7b8f7bb52efa94417a44b7b7f5
Krish120003 <krish120003@gmail.com>
2025-04-10 10:43:39 -0400

    fix: more loaders

--- src/app/[owner]/[repository]/(explorer)/blob/[branch]/[[...path]]/loading.tsx
+++ src/app/[owner]/[repository]/(explorer)/blob/[branch]/[[...path]]/loading.tsx
@@ -1,7 +1,53 @@
+"use client";
 import { File } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
 import FilenameFromParams from "./_components/filename";
 const skeletonWidths = [
+  "30%",
+  "45%",
+  "28%",
+  "40%",
+  "35%",
+  "37%",
+  "32%",
+  "42%",
+  "29%",
+  "50%",
+  "33%",
+  "41%",
+  "36%",
+  "27%",
+  "39%",
+  "30%",
+  "45%",
+  "28%",
+  "40%",
+  "35%",
+  "37%",
+  "32%",
+  "42%",
+  "29%",
+  "50%",
+  "33%",
+  "41%",
+  "36%",
+  "27%",
+  "39%",
+  "30%",
+  "45%",
+  "28%",
+  "40%",
+  "35%",
+  "37%",
+  "32%",
+  "42%",
+  "29%",
+  "50%",
+  "33%",
+  "41%",
+  "36%",
+  "27%",
+  "39%",
   "30%",
   "45%",
   "28%",
@@ -25,7 +71,7 @@ export default function Loading() {
   return (
     <div className="rounded-lg border border-border bg-background flex flex-col">
       {/* Header section similar to FolderView */}
-      <div className="flex border-b border-border justify-between">
+      <div className="flex border-b border-border justify-between ">
         <div className="p-3 text-muted-foreground font-medium flex gap-2 items-center">
           <File className="h-5 w-5 text-muted-foreground" />
           <FilenameFromParams />
