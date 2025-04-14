59fbdef15f749a3c0571477a6d5021dd406688b9
Krish120003 <krish120003@gmail.com>
2025-04-14 03:21:48 -0400

    feat: basic branch selector

--- src/app/[owner]/[repository]/(explorer)/layout.tsx
+++ src/app/[owner]/[repository]/(explorer)/layout.tsx
@@ -1,5 +1,6 @@
 import { api } from "@/trpc/server";
 import { FileTree } from "./_components/file-tree";
+import { BranchSelector } from "./_components/branch-selector";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -24,10 +25,17 @@ export default async function ExplorerLayout({
 
   return (
     <div className="grid grid-cols-12 w-full h-screen">
-      <div className="col-span-3 border-r border-foreground/20 overflow-y-auto">
-        <FileTree initialData={fileTreeInitial} />
+      <div className="col-span-3 border-r border-foreground/20 overflow-y-auto flex flex-col">
+        <div className="border-b border-foreground/20 flex flex-col items-stretch gap-2 p-2">
+          Branch
+          <BranchSelector />
+        </div>
+
+        <div className="flex-grow overflow-y-auto">
+          <FileTree initialData={fileTreeInitial} />
+        </div>
       </div>
-      <div className="col-span-9 p-4">{children}</div>
+      <div className="col-span-9 p-4 overflow-y-auto">{children}</div>
     </div>
   );
 }
