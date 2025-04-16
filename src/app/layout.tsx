a0d64c35067d311547d553eb8fca737683e8adf5
Krish120003 <krish120003@gmail.com>
2025-04-16 03:56:07 -0400

    chore: add nuqs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 
 import { TRPCReactProvider } from "@/trpc/react";
 import { SessionProvider } from "next-auth/react";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export const metadata: Metadata = {
   title: "Gitfaster",
@@ -20,16 +21,18 @@ export default function RootLayout({
       <head></head>
       <body className="min-h-screen bg-accent text-foreground antialiased">
         <SessionProvider>
-          <TRPCReactProvider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              {children}
-            </ThemeProvider>
-          </TRPCReactProvider>
+          <NuqsAdapter>
+            <TRPCReactProvider>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+              >
+                {children}
+              </ThemeProvider>
+            </TRPCReactProvider>
+          </NuqsAdapter>
         </SessionProvider>
       </body>
     </html>
